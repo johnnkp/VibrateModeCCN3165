2d61ff7fa1a4c71f0b6e2026798a54a42157474f
johnnkp <johnnkp@hotmail.com>
2019-02-21 17:50:16 +0800

    Tutorial 4 Page 12-15

--- app/src/main/java/hkcc/ccn3165/vibratemode/AppWidget.java
+++ app/src/main/java/hkcc/ccn3165/vibratemode/AppWidget.java
@@ -1,43 +1,59 @@
 package hkcc.ccn3165.vibratemode;
 
+import android.app.Activity;
+import android.app.IntentService;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
 import android.widget.RemoteViews;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class AppWidget extends AppWidgetProvider {
-
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.app_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
-
-    @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+    public void onReceive(Context ctxt, Intent intent) {
+        if (intent.getAction() == null) {
+            ctxt.startService(new Intent(ctxt, ToggleService.class));
+        } else {
+            super.onReceive(ctxt, intent);
         }
     }
 
     @Override
-    public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        context.startService(new Intent(context, ToggleService.class));
     }
 
-    @Override
-    public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
+    public static class ToggleService extends IntentService {
+        public ToggleService() {
+            super("AppWidget$ToggleService");
+        }
+
+        @Override
+        protected void onHandleIntent(Intent intent) {
+            ComponentName me = new ComponentName(this, AppWidget.class);
+            AppWidgetManager mgr = AppWidgetManager.getInstance(this);
+            mgr.updateAppWidget(me, buildUpdate(this));
+        }
+
+        private RemoteViews buildUpdate(Context context) {
+            RemoteViews updateViews = new RemoteViews(context.getPackageName(), R.layout.widget);
+            AudioManager audioManager = (AudioManager) context.getSystemService(Activity.AUDIO_SERVICE);
+            if (audioManager.getRingerMode() == AudioManager.RINGER_MODE_VIBRATE) {
+                updateViews.setImageViewResource(R.id.phoneState, R.drawable.phone_state_normal);
+                audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+            } else {
+                updateViews.setImageViewResource(R.id.phoneState, R.drawable.phone_state_vibrate);
+                audioManager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+            }
+            Intent i = new Intent(this, AppWidget.class);
+            PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
+            updateViews.setOnClickPendingIntent(R.id.phoneState, pi);
+            return updateViews;
+        }
     }
 }
-
